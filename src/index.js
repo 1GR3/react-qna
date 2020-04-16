d6adf1c66d97b145419d0a5721a7a6cf1fbcf3e1
Ivo Gregurec <ivo.gregurec@gmail.com>
2020-04-16 10:09:22 +0200

    code cleanup

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-
-
 import'bootstrap/dist/css/bootstrap.min.css';
 import $ from'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-
-/*
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Dropdown/>,document.getElementById('root'));
-registerServiceWorker();
-*/
-
-
 
 import './index.css';
 import App from './App';
