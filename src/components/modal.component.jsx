ddd602206a82eef80ef1a28ca9bc077383e14558
Ivo Gregurec <ivo.gregurec@gmail.com>
2020-04-23 11:26:51 +0200

    modals component

--- src/components/modal.component.jsx
+++ src/components/modal.component.jsx
@@ -14,3 +14,38 @@
       </div>
     </div>
   </div>
+  
+  <div className="modal fade" id="previewModal" tabIndex={-1} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h3>Preview</h3>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <div className="custom-control custom-radio">
+            <input type="radio" id="McustomRadio1" name="customRadio6" className="custom-control-input" />
+            <label className="custom-control-label" htmlFor="McustomRadio1">Preview radio A</label>
+          </div>
+          <div className="custom-control custom-radio">
+            <input type="radio" id="McustomRadio2" name="customRadio6" className="custom-control-input" />
+            <label className="custom-control-label" htmlFor="McustomRadio2">Preview radio B</label>
+          </div>
+          <div className="custom-control custom-radio">
+            <input type="radio" id="McustomRadio3" name="customRadio6" className="custom-control-input" />
+            <label className="custom-control-label" htmlFor="McustomRadio3">Preview radio C</label>
+          </div>
+          <br />
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="McustomCheck1" />
+            <label className="custom-control-label" htmlFor="McustomCheck1">Checkbox text</label>
+          </div>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-xs btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
